d25ea161195849fd54514e870b09359ef01971c1
Ashley Marlow <ashley.marlow@hotmail.com>
2022-10-04 18:47:45 -0700

    done!!!
    I think?

--- js/scripts.js
+++ js/scripts.js
@@ -15,30 +15,30 @@ async function handleSubmit(event) {
     }).then(response => {
         if (response.ok) {
             status.innerHTML = "Thanks for your submission!";
-            form.reset()
+            form.reset();
             status.style.display = "block";
             innerForm.style.display = "none";
         } else {
             response.json().then(data => {
             if (Object.hasOwn(data, 'errors')) {
-                status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+                status.innerHTML = data["errors"].map(error => error["message"]).join(", ");
             } else {
-                status.innerHTML = "Oops! There was a problem submitting your form"
+                status.innerHTML = "Oops! There was a problem submitting your form";
             }
             })
         }
     }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
+        status.innerHTML = "Oops! There was a problem submitting your form";
     });
 }
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
 
 
 // BACK TO TOP BUTTON
 let mybutton = document.getElementById("back-to-top");
 
 window.onscroll = function() {
-    scrollFunction()
+    scrollFunction();
 };
 
 function scrollFunction() {
