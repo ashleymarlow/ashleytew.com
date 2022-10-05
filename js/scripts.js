2c5a3e7d2c59a8c7a4d214cb42c29a6f84369149
Ashley Marlow <ashley.marlow@hotmail.com>
2022-10-04 17:49:37 -0700

    styling completed
    still need to work on portfolio mockups

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,52 @@
+// FORM SUBMISSION
 var form = document.getElementById("my-form");
 
 async function handleSubmit(event) {
     event.preventDefault();
     var status = document.getElementById("my-form-status");
     var data = new FormData(event.target);
+    var innerForm = document.getElementById("inner-form");
     fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-        'Accept': 'application/json'
-    }
-    }).then(response => {
-    if (response.ok) {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset()
-    } else {
-        response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-        } else {
-            status.innerHTML = "Oops! There was a problem submitting your form"
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
         }
-        })
-    }
+    }).then(response => {
+        if (response.ok) {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset()
+            status.style.display = "block";
+            innerForm.style.display = "none";
+        } else {
+            response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+                status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+                status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+            })
+        }
     }).catch(error => {
-    status.innerHTML = "Oops! There was a problem submitting your form"
+        status.innerHTML = "Oops! There was a problem submitting your form"
     });
 }
 form.addEventListener("submit", handleSubmit)
+
+
+// BACK TO TOP BUTTON
+let mybutton = document.getElementById("back-to-top");
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 420 || document.documentElement.scrollTop > 420) {
+    // mybutton.style.display = "flex";
+    mybutton.style.opacity = "1";
+  } else {
+    // mybutton.style.display = "none";
+    mybutton.style.opacity = "0";
+  }
+}
